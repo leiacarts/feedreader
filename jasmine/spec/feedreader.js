d00e59561f06f2d570a8e1cfdd8299eb3104c71e
Leia Carter <leiacarts@gmail.com>
2018-08-26 14:04:18 -0400

    menu text specs complete

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -58,26 +58,35 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-      describe('menu', function() {
-
+      describe('Menu', function() {
+        const body = document.querySelector('body');
+        const menu = document.querySelector('.menu-icon-link');
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
 
+         it('hidden by default', function() {
+           expect(body.classList.contains('menu-hidden')).toBe(true);
+         });
+
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-
+          it('changes visibility on click', function() {
+            menu.click();
+            expect(body.classList.contains('menu-hidden')).toBe(false);
+          });
 
 });
 
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
+      describe('Initial Entries', function() {});
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
